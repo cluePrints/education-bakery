831e253ef44caa4f34279cb72df3a1ebb7182086
some.unique.login <some.unique.login@b3896b12-28c4-11de-bcab-dfcfbda92187>
2009-05-01 21:37:32 +0000

    MoneyMoveSvc requirements

--- bakery_java/src/org/bakery/server/controllers/svc/impl/ProductMoveSvc.java
+++ bakery_java/src/org/bakery/server/controllers/svc/impl/ProductMoveSvc.java
@@ -5,6 +5,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.bakery.server.controllers.svc.SvcController;
 import org.bakery.server.controllers.svc.beans.AbstractFormMode;
+import org.bakery.server.controllers.svc.helper.SvcHelper;
 import org.bakery.server.domain.log.ProductMove;
 
 public class ProductMoveSvc extends AbstractCommand {
@@ -18,6 +19,7 @@ public class ProductMoveSvc extends AbstractCommand {
 
 	protected void executeInternal(HttpServletRequest request,
 			HttpServletResponse response, SvcController controller, AbstractFormMode mode) throws Exception {
-	
+		SvcHelper.write(response.getWriter(), controller.getDAOFacade().getWarehouseDAO(), "warehouses");
+		SvcHelper.write(response.getWriter(), controller.getDAOFacade().getMoneyMoveDAO(), "moneyMoves");
 	}
 }
