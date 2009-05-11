36f8d5882aced2c64c29e5289dfab59432717e1b
some.unique.login <some.unique.login@b3896b12-28c4-11de-bcab-dfcfbda92187>
2009-05-11 19:59:10 +0000

    Trigger recip_parameters fix when list is empty

--- bakery_java/WebContent/WEB-INF/DB/DDL 2009-05-11 21-00.sql
+++ bakery_java/WebContent/WEB-INF/DB/DDL 2009-05-11 21-00.sql
@@ -424,7 +424,7 @@ DROP TRIGGER IF EXISTS before_upd_recip_parameters|
 CREATE TRIGGER before_upd_recip_parameters
   BEFORE UPDATE ON recip_parameters FOR EACH ROW
   BEGIN
-  	DECLARE p INTEGER default 0;
+  	DECLARE p INTEGER default null;
   	SELECT device_parameter_id INTO p FROM device_parameters
 	WHERE device_parameter_id=1 AND device_parameters.device_id IN
 		(SELECT device_parameters.device_id FROM recip_parameters
@@ -442,7 +442,7 @@ DROP TRIGGER IF EXISTS before_ins_recip_parameters|
 CREATE TRIGGER before_ins_recip_parameters
   BEFORE INSERT ON recip_parameters FOR EACH ROW
   BEGIN
-  	DECLARE p INTEGER default 0;
+  	DECLARE p INTEGER default null;
   	SELECT device_parameter_id INTO p FROM device_parameters
 	WHERE device_parameter_id=1 AND device_parameters.device_id IN
 		(SELECT device_parameters.device_id FROM recip_parameters
