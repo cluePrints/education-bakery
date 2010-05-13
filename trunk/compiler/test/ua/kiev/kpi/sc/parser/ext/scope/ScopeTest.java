1234b507c23e845090967febcf963463f11bf7bb
some.unique.login <some.unique.login@b3896b12-28c4-11de-bcab-dfcfbda92187>
2010-05-13 19:03:00 +0000

    Improve scoping checking and building according to unittests(actually Fedors hint) - scope could contain variable and function with same name, parent and child could contain similarly named var

--- trunk/compiler/test/ua/kiev/kpi/sc/parser/ext/scope/ScopeTest.java
+++ trunk/compiler/test/ua/kiev/kpi/sc/parser/ext/scope/ScopeTest.java
@@ -1,5 +1,7 @@
 package ua.kiev.kpi.sc.parser.ext.scope;
 
+import java.util.LinkedList;
+
 import junit.framework.Assert;
 
 import org.junit.Test;
@@ -8,6 +10,8 @@ import ua.kiev.kpi.sc.parser.ext.id.FuncSymbol;
 import ua.kiev.kpi.sc.parser.ext.id.ScopeException;
 import ua.kiev.kpi.sc.parser.ext.id.TypeSymbol;
 import ua.kiev.kpi.sc.parser.ext.id.VarSymbol;
+
+import com.google.common.collect.Lists;
 
 public class ScopeTest {
 	@Test
@@ -21,8 +25,8 @@ public class ScopeTest {
 		VarSymbol childVar = new VarSymbol();
 		childVar.setName("a");
 		childScope.addIdentifier(childVar);
-		Assert.assertEquals(childVar, childScope.getVisibleSymbol("a"));
-		Assert.assertEquals(parentVar, parentScope.getVisibleSymbol("a"));
+		Assert.assertEquals(childVar, childScope.getVisibleVarSymbol("a"));
+		Assert.assertEquals(parentVar, parentScope.getVisibleVarSymbol("a"));
 	}
 	
 	@Test(expected=ScopeException.class)
@@ -46,9 +50,33 @@ public class ScopeTest {
 		FuncSymbol func1 = new FuncSymbol();
 		func1.setReturnType(returnType);
 		func1.setName("a");
+		func1.setParams(new LinkedList<VarSymbol>());
 		parentScope.addIdentifier(func1);
 		FuncSymbol func2 = new FuncSymbol();
 		func2.setName("a");
+		func2.setParams(new LinkedList<VarSymbol>());
+		func2.setReturnType(returnType);
+		parentScope.addIdentifier(func2);
+	}
+	
+	@SuppressWarnings("serial")
+	@Test
+	public void testDifferentArgsAllowed()
+	{
+		Scope parentScope = new RootScope();
+		TypeSymbol returnType = new TypeSymbol();
+		FuncSymbol func1 = new FuncSymbol();
+		func1.setReturnType(returnType);
+		func1.setName("a");
+		func1.setParams(new LinkedList<VarSymbol>());
+		parentScope.addIdentifier(func1);
+		FuncSymbol func2 = new FuncSymbol();
+		func2.setName("a");
+		func2.setParams(new LinkedList<VarSymbol>(){
+			{
+				add(new VarSymbol());
+			}
+		});
 		func2.setReturnType(returnType);
 		parentScope.addIdentifier(func2);
 	}
